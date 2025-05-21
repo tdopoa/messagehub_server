2e22b44f6c41a3abafe1ed41c39311895efeb013
Tiago Schulz <tiago.schulz@quartile.com>
2025-05-21 17:08:32 -0300

    Refactor WhatsApp chatbot module by renaming import and removing deprecated resource file to improve code organization and clarity.

--- src/v1/whatsapp_chatbots/__init__.py
+++ src/v1/whatsapp_chatbots/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from src.v1.whatsapp_chatbots.resource import whatsapp_chatbot
+from src.v1.whatsapp_chatbots.resource import whatsapp_chatbots
 
 api_router = APIRouter()
 
-api_router.include_router(router=whatsapp_chatbot.router)
+api_router.include_router(router=whatsapp_chatbots.router)
